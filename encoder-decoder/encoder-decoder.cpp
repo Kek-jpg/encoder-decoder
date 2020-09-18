80480ee0754d3753f5cfb820db18d043959bf657
Kek-jpg <Kek-jpg@users.noreply.github.com>
2020-09-18 22:31:25 +0500

    Шифратор и дешифратор разнесены по разным функциям, сделан пользовательский ввод

--- encoder-decoder/encoder-decoder.cpp
+++ encoder-decoder/encoder-decoder.cpp
@@ -4,13 +4,9 @@
 #include <algorithm>
 using namespace std;
 
-int main()
+string encode(string word)
 {
-    char alphabetical[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-   
-    string word = "soldier Zalu";
-    cout << word << endl;
-    transform(word.begin(), word.end(), word.begin(), ::tolower);
+    char alphabetical[26] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
     int k = 0;
     for (int i = 0; i < word.size(); i++)
     {
@@ -40,29 +36,34 @@ int main()
             }
         }
     }
-    cout << word << endl;
+    return word;
+}
+
+string decode(string word)
+{
+    char alphabetical[26] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
     transform(word.begin(), word.end(), word.begin(), ::tolower);
-    int s = 0;
+    int k = 0;
     for (int i = 0; i < word.size(); i++)
     {
         for (int j = 0; j < 26; j++)
         {
             if (word[i] == alphabetical[j])
             {
-                s = j - 3;
+                k = j - 3;
                 switch (j)
                 {
                 case 2:
-                    s = 25;
+                    k = 25;
                     break;
                 case 1:
-                    s = 24;
+                    k = 24;
                     break;
                 case 0:
-                    s = 23;
+                    k = 23;
                     break;
                 }
-                word[i] = alphabetical[s];
+                word[i] = alphabetical[k];
                 break;
             }
             else if (word[i] == ' ')
@@ -71,6 +72,30 @@ int main()
             }
         }
     }
-    cout << word << endl;
-    string result;
+    return word;
+}
+
+int main()
+{  
+    setlocale(LC_ALL, "ru");
+    int theSwitch;
+    string word;
+    cout << "Введите 1 для шифровки предолжения\nВведите 2 для расшифровки предложения\n";
+    cin >> theSwitch;
+    switch (theSwitch)
+    {
+    case 1:
+        cout << "Введите предложение для шифровки (на английском языке): ";
+        cin.get();
+        getline(cin, word);
+        cout << "Зашифрованное предложение: " + encode(word);
+        break;
+    case 2:
+        cout << "Введите предложение для расшифровки (на английском языке): ";
+        cin.get();
+        getline(cin, word);
+        cout << "Расшифрованное предложение: " + decode(word);
+        break;
+
+    }
 }
